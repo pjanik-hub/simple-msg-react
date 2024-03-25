f857a992b16fb1c93e2e205b1eaea4acac878fc8
Philip <philipjanik3@gmail.com>
2024-03-24 21:56:34 -0600

    added simple page (login in prog)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,42 @@
+"use client";
+
+import { FormEvent } from "react";
+
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 
+async function handleLogin(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    const response = await fetch("/api/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+    });
+}
+
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <Box
-        component="form"
-        display="flex"
-        flexDirection="column"
-        gap="8px"
-        noValidate
-        autoComplete="off"
-      >
-        <TextField id="email" label="Email" />
-        <TextField id="password" label="Password" />
-        <Button variant="contained">Login</Button>
-      </Box>
-    </main>
-  );
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-center p-24">
+            <h1 className="p-4">Simple Message</h1>
+            <Box
+                component="form"
+                display="flex"
+                flexDirection="column"
+                gap="8px"
+                onSubmit={handleLogin}
+                noValidate
+                autoComplete="off"
+            >
+                <TextField id="email" label="Email" required={true} />
+                <TextField id="password" label="Password" required={true} />
+                <Button type="submit">Login</Button>
+            </Box>
+        </main>
+    );
 }
